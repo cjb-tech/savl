d16e9e03590e3a577c9f06c020efd97a12c6db3b
Manzhula David <da411d@gmail.com>
2021-09-28 22:04:38 +0300

    Update sketch_aug14a.ino

--- sketch_aug14a.ino
+++ sketch_aug14a.ino
@@ -1,6 +1,4 @@
-#define START_SLEEP 5000
-#define SWITCH_INTERVAL 5000
-#define LOOP_DELAY 5000
+#define LOOP_DELAY 500
 
 #define ZERO 2
 #define X32 3
@@ -9,86 +7,85 @@
 #define TOP_R 6
 #define SUB_AND_MON_L 7
 #define SUB_AND_MON_R 8
-#define USELESS 9
+#define USELESS_1 9
+#define USELESS_2 10
 
-
-#define ZERO_ANALOG 3
-#define X32_ANALOG 5
-#define DI_ANALOG 6
-#define TOP_L_ANALOG 9
-#define TOP_R_ANALOG 11
-#define SUB_AND_MON_L_ANALOG 7
-#define SUB_AND_MON_R_ANALOG 8
-#define USELESS_ANALOG 9
+#define ZERO_ANALOG 15
+#define X32_ANALOG 16
+#define DI_ANALOG 17
+#define TOP_L_ANALOG 18
+#define TOP_R_ANALOG 19
+#define SUB_AND_MON_L_ANALOG 20
+#define SUB_AND_MON_R_ANALOG 21
+#define USELESS_1_ANALOG 0
+#define USELESS_2_ANALOG -1
 
 #define SWITCHER 13
 
-#define SIZE 8
+#define SIZE 9
 
 
-bool is_switcher_on(){
-  return true;//digitalRead(SWITCHER) == HIGH;
+bool is_switcher_on() {
+  return digitalRead(SWITCHER) == HIGH;
 }
 
-struct Pin{
-  
-  int num;
-  int analogNum;
-  int power_on_delay;
-  int power_off_delay;
+struct Pin {
+  int relay_pin;
+  bool repay_pin_inverted;
+  int led_pin;
 
-  void setup(){
-    pinMode(num, OUTPUT);
-    digitalWrite(num, HIGH);  
-    analogWrite(analogNum, 0);
-  }
-  
-  bool enabled(){
-    return digitalRead(num) == LOW;
+  int delay_on;
+  int delay_off;
+
+  void setup() {
+    pinMode(relay_pin, OUTPUT);
+    digitalWrite(relay_pin, repay_pin_inverted);
+
+    pinMode(led_pin, OUTPUT);
+    digitalWrite(led_pin, 0);
   }
 
-  void disable(){
-    if(!enabled()){
+  bool is_enabled() {
+    return digitalRead(relay_pin) != repay_pin_inverted;
+  }
+
+  void enable() {
+    if (is_enabled()) {
       return;
     }
 
-    delay(power_off_delay);
-    digitalWrite(num, HIGH);
-    analogWrite(analogNum, 255);
-   }
+    delay(delay_on / 5);
+    digitalWrite(relay_pin, !repay_pin_inverted);
+    digitalWrite(led_pin, 255);
+  }
 
-  void enable(){
-    if(enabled()){
+  void disable() {
+    if (!is_enabled()) {
       return;
     }
 
-    delay(power_on_delay);
-    digitalWrite(num, LOW);
-    analogWrite(analogNum, 0);
-   }
-
+    delay(delay_off / 5);
+    digitalWrite(relay_pin, repay_pin_inverted);
+    digitalWrite(led_pin, 0);
+  }
 };
-
-/*
- * pin, - number of pin on arduino
- * power on delay - delay after pin is enabled
- * power off delay - delay after pin is disabled
- */
 
 Pin pins[SIZE] = {
-  Pin{ZERO, ZERO_ANALOG, 2000, 2000},
-  Pin{X32, X32_ANALOG, 2000, 2000},
-  Pin{DI, DI_ANALOG, 2000, 2000},
-  Pin{TOP_L, TOP_L_ANALOG, 2000, 2000},
-  Pin{TOP_R, TOP_R_ANALOG, 2000, 2000},
-  Pin{SUB_AND_MON_L, SUB_AND_MON_L_ANALOG, 2000, 2000},
-  Pin{SUB_AND_MON_R, SUB_AND_MON_R_ANALOG, 2000, 2000},
-  Pin{USELESS, USELESS_ANALOG, 2000, 2000}
+  //{relay_pin_num, inverted, led_pin_num,      delay_on, delay_off }
+  {ZERO,            false,    ZERO_ANALOG,          2000, 2000},
+  {X32,             true,     X32_ANALOG,           2000, 2000},
+  {DI,              true,     DI_ANALOG,            2000, 2000},
+  {TOP_L,           true,     TOP_L_ANALOG,         2000, 2000},
+  {TOP_R,           true,     TOP_R_ANALOG,         2000, 2000},
+  {SUB_AND_MON_L,   true,     SUB_AND_MON_L_ANALOG, 2000, 2000},
+  {SUB_AND_MON_R,   true,     SUB_AND_MON_R_ANALOG, 2000, 2000},
+  {USELESS_1,       true,     USELESS_1_ANALOG,     2000, 2000},
+  {USELESS_2,       true,     USELESS_2_ANALOG,     2000, 2000}
 };
 
-void enable(){
-  for(int i=0; i<SIZE; i++){
-    if(!is_switcher_on()){
+void enable() {
+  for (int i = 0; i < SIZE; i++) {
+    if (!is_switcher_on()) {
       return;
     }
     Pin pin = pins[i];
@@ -96,9 +93,9 @@ void enable(){
   }
 }
 
-void disable(){
-  for(int i=SIZE-1; i>=0; i--){
-    if(is_switcher_on()){
+void disable() {
+  for (int i = SIZE - 1; i >= 0; i--) {
+    if (is_switcher_on()) {
       return;
     }
     Pin pin = pins[i];
@@ -107,24 +104,21 @@ void disable(){
 }
 
 void setup() {
-
   pinMode(SWITCHER, INPUT);
-  
-  for(int i=0; i<SIZE; i++){
+
+  for (int i = 0; i < SIZE; i++) {
     Pin pin = pins[i];
     pin.setup();
   }
-  
-  Serial.begin(9600);
 
-  delay(START_SLEEP);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if(is_switcher_on()){
+  delay(LOOP_DELAY);
+  if (is_switcher_on()) {
     enable();
-  }else{
+  } else {
     disable();
   }
-  delay(LOOP_DELAY);
 }
